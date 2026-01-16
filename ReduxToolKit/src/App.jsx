b73eddf8c3f94aaef50b3f78c10e4ab22dbd0be3
fawahkhan <khanmohdfawah21@gmail.com>
2026-01-16 20:37:59 +0530

    made a counterwith redux

--- ReduxToolKit/src/App.jsx
+++ ReduxToolKit/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import {useDispatch , useSelector} from 'react-redux'
+import { decrement, increment, incrementByAmount } from './redux/features/counterSlice'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  const counter = useSelector((state)=>state.counter.value)
+
+
+
   return (
     <div>
-      <h1>0 hello</h1>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>{
+          dispatch(increment())
+      }}>Increment</button>
+      <button onClick={()=>{
+          dispatch(decrement())
+      }}>Decrement</button>
+      <button onClick={()=>{
+        dispatch(incrementByAmount(10))
+      }}>Increment by Amount</button>
     </div>
   )
 }
